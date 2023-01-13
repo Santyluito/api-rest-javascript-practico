0b4e55b8c0a47a0fe5d22e2de420f8dd0ff42dd9
Santyluito <santyluito@gmail.com>
2023-01-13 12:23:52 -0500

    agregando movies y categories

--- src/main.js
+++ src/main.js
@@ -1,1 +1,43 @@
-console.log('API ')
+async function getTrendingMoviesPreview() {
+  const res = await fetch('https://api.themoviedb.org/3/trending/movie/day'+API_KEY);
+  const data = await res.json();
+
+  const movies = data.results;
+  movies.forEach(movie => {
+    const TrendigPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+    const movieContainer = document.createElement('div');
+    movieContainer.classList.add('movie-container');
+
+    const movieImg = document.createElement('img');
+    movieImg.classList.add('movie-img');
+    movieImg.setAttribute('alt',movie.title);
+    movieImg.setAttribute('src',`https://image.tmdb.org/t/p/w300${movie.poster_path}`);
+
+    movieContainer.appendChild(movieImg);
+    TrendigPreviewMoviesContainer.appendChild(movieContainer);
+  });
+}
+
+async function getCategoriesPreview() {
+  const res = await fetch('https://api.themoviedb.org/3/genre/movie/list'+API_KEY);
+  const data = await res.json();
+
+  const categories = data.genres;
+  categories.forEach(category => {
+    const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+    const categoryContainer = document.createElement('div');
+    categoryContainer.classList.add('category-container');
+
+    const categoryTitle = document.createElement('h3');
+    categoryTitle.classList.add('category-title');
+    categoryTitle.setAttribute('id',`id${category.id}`);
+    const categoryTitletext = document.createTextNode(category.name);
+
+    categoryTitle.appendChild(categoryTitletext);
+    categoryContainer.appendChild(categoryTitle);
+    previewCategoriesContainer.appendChild(categoryContainer);
+  });
+}
+
+getTrendingMoviesPreview();
+getCategoriesPreview();
